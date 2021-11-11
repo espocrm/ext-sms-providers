c2a239fc193428c65b2caf8a53f305cf8f5902a2
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2021-11-11 14:50:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,221 @@
+# Template repository for EspoCRM extensions
+
+Create a repository for your extension from this template.
+
+## Preparing repository
+
+Run:
+
+```
+php init.php
+```
+
+It will ask to enter an extension name and some other information.
+
+After that, you can remove `init.php` file from your respository. Commit changes and proceed to configuration & building.
+
+
+## Configuration
+
+Create `config.json` file in the root directory. You can copy `config-default.json` and rename it to `config.json`.
+
+When reading, this config will be merged with `config-default.json`. You can override default parameters in the created config.
+
+Parameters:
+
+* espocrm.repository - from what repository to fetch EspoCRM;
+* espocrm.branch - what branch to fetch (`stable` is set by default); you can specify version number instead (e.g. `5.9.2`);
+* database - credentials of the dev database;
+* install.siteUrl - site url of the dev instance;
+* install.defaultOwner - a webserver owner (important to be set right);
+* install.defaultGroup - a webserver group (important to be set right).
+
+
+## Config for EspoCRM instance
+
+You can override EspoCRM config. Create `config.php` in the root directory of the repository. This file will be applied after EspoCRM intallation (when building).
+
+Example:
+
+```php
+<?php
+return [
+    'useCacheInDeveloperMode' => true,
+];
+```
+
+## Building
+
+After building, EspoCRM instance with installed extension will be available at `site` directory. You will be able to access it with credentials:
+
+* Username: admin
+* Password: 1
+
+### Preparation
+
+1. You need to have *node*, *npm*, *composer* installed.
+2. Run `npm install`.
+3. Create a database. The database name is set in the config file.
+
+### Full EspoCRM instance building
+
+It will download EspoCRM (from the repository specified in the config), then build and install it. Then it will install the extension.
+
+Command:
+
+```
+node build --all
+```
+
+Note: It will remove a previously installed EspoCRM instance, but keep the database intact.
+
+### Copying extension files to EspoCRM instance
+
+You need to run this command every time you make changes in `src` directory and you want to try these changes on Espo instance.
+
+Command:
+
+```
+node build --copy
+```
+
+### Running after-install script
+
+AfterInstall.php will be applied for EspoCRM instance.
+
+Command:
+
+```
+node build --after-install
+```
+
+### Extension package building
+
+Command:
+
+```
+node build --extension
+```
+
+The package will be created in `build` directory.
+
+Note: The version number is taken from `package.json`.
+
+### Installing addition extensions
+
+If your extension requires other extensions, there is a way to install them automatically while building the instance.
+
+Necessary steps:
+
+1. Add the current EspoCRM version to the `config.php`:
+
+```php
+<?php
+return [
+    'version' => '6.2.0',
+];
+
+```
+
+2. Create the `extensions` directory in the root directory of your repository.
+3. Put needed extensions (e.g. `my-extension-1.0.0.zip`) in this directory.
+
+Extensions will be installed automatically after running the command `node build --all` or `node build --install`.
+
+## Development workflow
+
+1. Do development in `src` dir.
+2. Run `node build --copy`.
+3. Test changes in EspoCRM instance at `site` dir.
+
+## Blocking out extension in Espo
+
+You can block out new entity types right in Espo (using Entity Manager) and then copy generated custom files (`site/custom` dir) to the repository (`src` dir) using `copy-custom.js` script.
+
+1. Create entity types, fields, layouts, relationships in Espo (it should be available in `site` dir after building).
+2. Run `node copy-custom.js`. It will copy all files from `site/custom` to `src/files/application/Modules/{ModuleName}` and apply needed modifications to files.
+3. Remove files from `site/custom`.
+4. Run `node build --copy`. It will copy files from the repository to Espo build (`site/application/Espo/Modules/{ModuleName}` dir).
+5. Clear cache in Espo.
+6. Test in Espo.
+7. Commit changes.
+8. Profit.
+
+You can remove `copy-custom.js` from the repository if you don't plan to use it future.
+
+## Using composer in extension
+
+If your extension requires to use additional libraries, they can be installed by the composer:
+
+1. Create a file `src/files/application/Modules/{ModuleName}/composer.json` with your dependencies.
+2. Once you run `node build --all` or `node build --composer-install`, composer dependencies will be automatically installed.
+
+Note: the extension build will contain only the `vendor` directory without `composer.json` file.
+
+## Versioning
+
+The version number is stored in `package.json` and `package-lock.json`.
+
+Bumping version:
+
+```
+npm version patch
+npm version minor
+npm version major
+```
+
+## Tests
+
+Prepare:
+
+1. `node build --copy`
+2. `cd site`
+3. `grunt test`
+
+### Unit
+
+Command to run unit tests:
+
+```
+vendor/bin/phpunit tests/unit/Espo/Modules/{@name}
+```
+
+### Integration
+
+You need to create a config file `tests/integration/config.php`:
+
+```php
+<?php
+
+return [
+    'database' => [
+        'driver' => 'pdo_mysql',
+        'host' => 'localhost',
+        'charset' => 'utf8mb4',
+        'dbname' => 'TEST_DB_NAME',
+        'user' => 'YOUR_DB_USER',
+        'password' => 'YOUR_DB_PASSWORD',
+    ],
+];
+```
+The file should exist before you run `node build --copy`.
+
+Command to run integration tests:
+
+```
+vendor/bin/phpunit tests/integration/Espo/Modules/{@name}
+```
+
+## Configuring IDE
+
+You need to set the following paths to be ignored in your IDE:
+
+* `build`
+* `site/build`
+* `site/application/Espo/Modules/{@name}`
+* `site/tests/unit/Espo/Modules/{@name}`
+* `site/tests/integration/Espo/Modules/{@name}`
+
+## License
+
+Change a license in `LICENSE` file. The current license is intended for scripts of this repository. It's not supposed to be used for code of your extension.
---
+++ build.js
@@ -0,0 +1,458 @@
+const fs = require('fs-extra');
+const unzipper = require('unzipper');
+const mv = require('mv');
+const cp = require('child_process');
+const path = require('path');
+const request = require('request');
+
+const helpers = require('./helpers.js');
+const extensionParams = require('./extension.json');
+
+const config = helpers.loadConfig();
+
+const branch = helpers.getProcessParam('branch');
+
+if (helpers.hasProcessParam('all')) {
+    fetchEspo({branch: branch})
+    .then(
+        () => install()
+    )
+    .then(
+        () => installExtensions()
+    )
+    .then(
+        () => copyExtension()
+    )
+    .then(
+        () => composerInstall()
+    )
+    .then(
+        () => rebuild()
+    )
+    .then(
+        () => afterInstall()
+    )
+    .then(
+        () => setOwner()
+    )
+    .then(
+        () => console.log('Done')
+    );
+}
+
+if (helpers.hasProcessParam('install')) {
+    install().then(function () {
+        installExtensions().then(function () {
+            setOwner().then(function () {
+                console.log('Done');
+            });
+        });
+    });
+}
+
+if (helpers.hasProcessParam('fetch')) {
+    fetchEspo({branch: branch}).then(function () {
+        console.log('Done');
+    });
+}
+
+if (helpers.hasProcessParam('copy')) {
+    copyExtension().then(function () {
+        setOwner().then(function () {
+            console.log('Done');
+        });
+    });
+}
+if (helpers.hasProcessParam('after-install')) {
+    afterInstall().then(function () {
+        console.log('Done');
+    });
+}
+
+if (helpers.hasProcessParam('extension')) {
+    buildExtension().then(function () {
+        console.log('Done');
+    });
+}
+
+if (helpers.hasProcessParam('rebuild')) {
+    rebuild().then(function () {
+        console.log('Done');
+    });
+}
+
+if (helpers.hasProcessParam('composer-install')) {
+    composerInstall().then(function () {
+        console.log('Done');
+    });
+}
+
+function fetchEspo (params) {
+    params = params || {};
+
+    return new Promise(function (resolve, fail) {
+        console.log('Fetching EspoCRM repository...');
+
+        if (fs.existsSync('./site/archive.zip')) {
+            fs.unlinkSync('./site/archive.zip');
+        }
+
+        helpers.deleteDirRecursively('./site');
+
+        if (!fs.existsSync('./site')) {
+            fs.mkdirSync('./site');
+        }
+
+        var branch = params.branch || config.espocrm.branch;
+
+        if (config.espocrm.repository.indexOf('https://github.com') === 0) {
+            var repository = config.espocrm.repository;
+
+            if (repository.substr(-4) === '.git') {
+                repository = repository.substr(0, repository.length - 4);
+            }
+
+            if (repository.substr(-1) !== '/') repository += '/';
+
+            var archiveUrl = repository + 'archive/' + branch + '.zip';
+
+            console.log('  Downloading EspoCRM archive from Github...');
+
+            request(archiveUrl)
+                .pipe(fs.createWriteStream('./site/archive.zip'))
+                .on('close', function () {
+                    console.log('  Unzipping...');
+
+                    fs.createReadStream('./site/archive.zip')
+                        .pipe(
+                            unzipper.Extract({path: 'site'})
+                        ).on('close', function () {
+                            fs.unlinkSync('./site/archive.zip');
+
+                            helpers.moveDir('./site/espocrm-' + branch.replace('/', '-'), './site').then(function () {
+                                resolve();
+                            });
+                        }).on('error', function () {
+                            console.log('  Error while unzipping.');
+                            fail();
+                        });
+                });
+        }
+        else {
+            // var command = "git archive --remote=\""+repository+"\" --output=\"./site/archive.zip\" " + branch;
+        }
+    });
+}
+
+function install () {
+    return new Promise(function (resolve) {
+        console.log('Installing EspoCRM instance...');
+
+        console.log('  Creating config...');
+
+        createConfig();
+
+        buildEspo();
+
+        if (fs.existsSync('./site/install/config.php')) {
+            fs.unlinkSync('./site/install/config.php');
+        }
+
+        console.log('  Install: step1...');
+
+        cp.execSync("php install/cli.php -a step1 -d \"user-lang=" + config.install.language + "\"",
+            {cwd: './site'});
+
+        console.log('  Install: setupConfirmation...');
+
+        cp.execSync(
+            "php install/cli.php -a setupConfirmation -d \"host-name=" + config.database.host +
+            "&db-name=" + config.database.dbname +
+            "&db-user-name=" + config.database.user +
+            "&db-user-password=" + config.database.password + "\"",
+            {cwd: './site'}
+        );
+
+        console.log('  Install: checkPermission...');
+
+        cp.execSync("php install/cli.php -a \"checkPermission\"", {cwd: './site', stdio: 'ignore'});
+
+        console.log('  Install: saveSettings...');
+
+        cp.execSync(
+            "php install/cli.php -a saveSettings -d \"site-url=" + config.install.siteUrl +
+            "&default-permissions-user=" + config.install.defaultOwner +
+            "&default-permissions-group=" + config.install.defaultGroup + "\"",
+            {cwd: './site'}
+        );
+
+        console.log('  Install: buildDatabase...');
+
+        cp.execSync("php install/cli.php -a \"buildDatabase\"", {cwd: './site', stdio: 'ignore'});
+
+        console.log('  Install: createUser...');
+
+        cp.execSync("php install/cli.php -a createUser -d \"user-name=" + config.install.adminUsername +
+            '&user-pass=' + config.install.adminPassword + "\"",
+            {cwd: './site'}
+        );
+
+        console.log('  Install: finish...');
+
+        cp.execSync("php install/cli.php -a \"finish\"", {cwd: './site'});
+
+        console.log('  Merge configs...');
+
+        cp.execSync("php merge_configs.php", {cwd: './php_scripts'});
+
+        resolve();
+    });
+}
+
+function buildEspo () {
+    console.log('  Npm install...');
+
+    cp.execSync("npm install", {cwd: './site', stdio: 'ignore'});
+
+    console.log('  Building...');
+
+    cp.execSync("grunt", {cwd: './site', stdio: 'ignore'});
+}
+
+function createConfig () {
+    const config = helpers.loadConfig();
+
+    var configString = `<?php
+        return [
+            'database' => [
+                'driver' => '${config.database.driver}',
+                'host' => '${config.database.host}',
+                'port' => '${config.database.port}',
+                'charset' => '${config.database.charset}',
+                'dbname' => '${config.database.dbname}',
+                'user' => '${config.database.user}',
+                'password' => '${config.database.password}',
+            ],
+            'isDeveloperMode' => true,
+            'useCache' => true,
+        ];
+    `;
+
+    fs.writeFileSync('./site/data/config.php', configString);
+}
+
+function copyExtension () {
+    return new Promise(function (resolve, fail) {
+        console.log('Copying extension to EspoCRM instance...');
+
+        var moduleName = extensionParams.module;
+        var moduleNameHyphen = helpers.camelCaseToHyphen(moduleName);
+
+        if (fs.existsSync('./site/application/Espo/Modules/' + moduleName)) {
+            console.log('  Removing backend files...');
+
+            helpers.deleteDirRecursively('./site/application/Espo/Modules/' + moduleName);
+        }
+
+        if (fs.existsSync('./site/client/modules/' + moduleNameHyphen)) {
+            console.log('  Removing frontend files...');
+
+            helpers.deleteDirRecursively('./site/client/modules/' + moduleNameHyphen);
+        }
+
+        if (fs.existsSync('./site/tests/unit/Espo/Modules/' + moduleName)) {
+            console.log('  Removing unit test files...');
+
+            helpers.deleteDirRecursively('./site/tests/unit/Espo/Modules/' + moduleName);
+        }
+
+        if (fs.existsSync('./site/tests/integration/Espo/Modules/' + moduleName)) {
+            console.log('  Removing integration test files...');
+
+            helpers.deleteDirRecursively('./site/tests/integration/Espo/Modules/' + moduleName);
+        }
+
+        console.log('  Copying files...');
+
+        fs.copySync('./src/files', './site/');
+        fs.copySync('./tests', './site/tests');
+
+        resolve();
+    });
+}
+
+function rebuild () {
+    return new Promise(function (resolve) {
+        console.log('Rebuilding EspoCRM instance...');
+
+        cp.execSync("php rebuild.php", {cwd: './site'});
+
+        resolve();
+    });
+}
+
+function afterInstall () {
+    return new Promise(function (resolve) {
+        console.log('Running after-install script...');
+
+        cp.execSync("php after_install.php", {cwd: './php_scripts'});
+
+        resolve();
+    })
+}
+
+function buildExtension () {
+    return new Promise(function (resolve) {
+        console.log('Building extension package...');
+
+        var moduleName = extensionParams.module;
+        var moduleNameHyphen = helpers.camelCaseToHyphen(moduleName);
+
+        const package = require('./package.json');
+
+        var manifest = {
+            name: extensionParams.name,
+            description: extensionParams.description,
+            author: extensionParams.author,
+            php: extensionParams.php,
+            acceptableVersions: extensionParams.acceptableVersions,
+            version: package.version,
+            skipBackup: true,
+            releaseDate: (new Date()).toISOString().split('T')[0],
+        };
+
+        var packageFileName = moduleNameHyphen + '-' + package.version + '.zip';
+
+        if (!fs.existsSync('./build')) {
+            fs.mkdirSync('./build');
+        }
+
+        if (fs.existsSync('./build/tmp')) {
+            helpers.deleteDirRecursively('./build/tmp');
+        }
+
+        if (fs.existsSync('./build/' + packageFileName)) {
+            fs.unlinkSync('./build/' + packageFileName);
+        }
+
+        fs.mkdirSync('./build/tmp');
+
+        fs.copySync('./src', './build/tmp');
+
+        internalComposerBuildExtension();
+
+        fs.writeFileSync('./build/tmp/manifest.json', JSON.stringify(manifest, null, 4));
+
+        const archiver = require('archiver');
+        const archive = archiver('zip');
+
+        var zipOutput = fs.createWriteStream('./build/' + packageFileName);
+
+        zipOutput.on('close', function () {
+            console.log('Package has been built.');
+
+            helpers.deleteDirRecursively('./build/tmp');
+
+            resolve();
+        });
+
+        var currentPath = path.dirname(fs.realpathSync(__filename));
+
+        archive.directory('./build/tmp', '').pipe(zipOutput);
+
+        archive.finalize();
+    });
+}
+
+function installExtensions () {
+    return new Promise(function (resolve, fail) {
+
+        if (!fs.existsSync('./extensions')) {
+            resolve();
+
+            return;
+        }
+
+        console.log("Installing extensions from 'extensions' directory...");
+
+        fs.readdirSync('./extensions/').forEach( function (file) {
+            if (path.extname(file).toLowerCase() != '.zip') {
+                return;
+            }
+
+            console.log('  Install: ' + file);
+
+            cp.execSync(
+                "php command.php extension --file=\"../extensions/" + file + "\"",
+                {
+                    cwd: './site',
+                    stdio: 'ignore',
+                }
+            );
+        });
+
+        resolve();
+    });
+}
+
+function setOwner () {
+    return new Promise(function (resolve) {
+        try {
+            cp.execSync(
+                "chown -R " + config.install.defaultOwner + ":" + config.install.defaultGroup + " .",
+                {
+                    cwd: './site',
+                    stdio: 'ignore',
+                }
+            );
+        }
+        catch (e) {}
+
+        resolve();
+    });
+}
+
+function composerInstall () {
+    return new Promise(function (resolve, fail) {
+        var moduleName = extensionParams.module;
+
+        internalComposerInstall('./site/application/Espo/Modules/' + moduleName);
+
+        resolve();
+    });
+}
+
+function internalComposerInstall (modulePath) {
+    if (!fs.existsSync(modulePath + '/composer.json')) {
+
+        return;
+    }
+
+    console.log('Running composer install...');
+
+    cp.execSync(
+        "composer install --no-dev --ignore-platform-reqs",
+        {
+            cwd: modulePath,
+            stdio: 'ignore'
+        }
+    );
+}
+
+function internalComposerBuildExtension() {
+    var moduleName = extensionParams.module;
+
+    internalComposerInstall('./build/tmp/files/application/Espo/Modules/' + moduleName);
+
+    var removedFileList = [
+        'files/application/Espo/Modules/' + moduleName + '/composer.json',
+        'files/application/Espo/Modules/' + moduleName + '/composer.lock',
+        'files/application/Espo/Modules/' + moduleName + '/composer.phar',
+    ];
+
+    removedFileList.forEach(file => {
+        if (fs.existsSync('./build/tmp/' + file)) {
+            fs.unlinkSync('./build/tmp/' + file);
+        }
+    });
+}
