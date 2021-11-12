db5a7e325c6786ea123c913391af0dbc61eb6db4
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2021-11-12 11:45:48 +0200

    spryng

---
+++ src/files/custom/Espo/Modules/SmsProviders/Spryng/SpryngSender.php
@@ -0,0 +1,190 @@
+<?php
+/************************************************************************
+ * This file is part of EspoCRM.
+ *
+ * EspoCRM - Open Source CRM application.
+ * Copyright (C) 2014-2021 Yurii Kuznietsov, Taras Machyshyn, Oleksii Avramenko
+ * Website: https://www.espocrm.com
+ *
+ * EspoCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EspoCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
+ ************************************************************************/
+
+namespace Espo\Modules\SmsProviders\Spryng;
+
+use Espo\Core\Sms\Sender;
+use Espo\Core\Sms\Sms;
+
+use Espo\Core\Utils\Config;
+use Espo\Core\Utils\Log;
+use Espo\Core\Utils\Json;
+use Espo\Core\Exceptions\Error;
+
+use Espo\ORM\EntityManager;
+
+use Espo\Entities\Integration;
+
+use Throwable;
+
+class SpryngSender implements Sender
+{
+    private const BASE_URL = 'https://rest.spryngsms.com/v1';
+
+    private const TIMEOUT = 10;
+
+    private $config;
+
+    private $entityManager;
+
+    private $log;
+
+    public function __construct(Config $config, EntityManager $entityManager, Log $log)
+    {
+        $this->config = $config;
+        $this->entityManager = $entityManager;
+        $this->log = $log;
+    }
+
+    public function send(Sms $sms): void
+    {
+        $toNumberList = $sms->getToNumberList();
+
+        if (!count($toNumberList)) {
+            throw new Error("No recipient phone number.");
+        }
+
+        foreach ($toNumberList as $number) {
+            $this->sendToNumber($sms, $number);
+        }
+    }
+
+    private function sendToNumber(Sms $sms, string $toNumber): void
+    {
+        $integration = $this->getIntegrationEntity();
+
+        $authToken = $integration->get('spryngAuthToken');
+        $baseUrl = rtrim(
+            $integration->get('apiBaseUrl') ??
+            $this->config->get('spryngApiBaseUrl') ??
+            self::BASE_URL
+        );
+
+        $encoding = $integration->get('spryngEncoding');
+        $originator = $integration->get('spryngOriginator');
+        $route = $integration->get('spryngRoute');
+
+        $timeout = $this->config->get('spryngSmsSendTimeout') ?? self::TIMEOUT;
+
+        if (!$authToken) {
+            throw new Error("No Spryng Auth Token.");
+        }
+
+        if (!$toNumber) {
+            throw new Error("No recipient phone number.");
+        }
+
+        $url = $baseUrl . '/messages';
+
+        $data = [
+            'encoding' => $encoding,
+            'route' => $route,
+            'originator' => $originator,
+            'body' => $sms->getBody(),
+            'recipients' => [self::formatNumber($toNumber)],
+        ];
+
+        $headers = [
+            'Authorization: Bearer ' . $authToken,
+            'Content-Type: application/json',
+            'Accept: application/json',
+        ];
+
+        $ch = curl_init();
+
+        curl_setopt($ch, \CURLOPT_URL, $url);
+        curl_setopt($ch, \CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, \CURLOPT_HEADER, true);
+        curl_setopt($ch, \CURLOPT_TIMEOUT, $timeout);
+        curl_setopt($ch, \CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($ch, \CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, \CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, \CURLOPT_POSTFIELDS, $this->buildQuery($data));
+
+        $GLOBALS['log']->warning(json_encode( $this->buildQuery($data)) );
+
+        $response = curl_exec($ch);
+        $code = curl_getinfo($ch, \CURLINFO_HTTP_CODE);
+        $error = curl_errno($ch);
+
+        $headerSize = curl_getinfo($ch, \CURLINFO_HEADER_SIZE);
+
+        $body = mb_substr($response, $headerSize);
+
+        if ($code && !($code >= 200 && $code < 300)) {
+            $this->processError($code, $body);
+        }
+
+        if ($error) {
+            if (in_array($error, [\CURLE_OPERATION_TIMEDOUT, \CURLE_OPERATION_TIMEOUTED])) {
+                throw new Error("Spryng SMS sending timeout.");
+            }
+        }
+    }
+
+    private function buildQuery(array $data): string
+    {
+        return json_encode($data);
+    }
+
+    private static function formatNumber(string $number): string
+    {
+        return '+' . preg_replace('/[^0-9]/', '', $number);
+    }
+
+    private function processError(int $code, string $body): void
+    {
+        try {
+            $data = Json::decode($body);
+
+            $message = $data->message ?? null;
+        }
+        catch (Throwable $e) {
+            $message = null;
+        }
+
+        if ($message) {
+            $this->log->error("Spryng SMS sending error. Message: " . $message);
+        }
+
+        throw new Error("Spryng SMS sending error. Code: {$code}.");
+    }
+
+    private function getIntegrationEntity(): Integration
+    {
+        $entity = $this->entityManager
+            ->getEntity(Integration::ENTITY_TYPE, 'Spryng');
+
+        if (!$entity || !$entity->get('enabled')) {
+            throw new Error("Spryng integration is not enabled");
+        }
+
+        return $entity;
+    }
+}
